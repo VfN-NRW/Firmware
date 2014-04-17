a728575ce09bd9a5baad6c90ce4074624ed8fb7b
Freifunk Firmware <firmware@liztv.net>
2014-04-17 08:09:18 +0200

    * added ebtables-options to compile * added debuginfo to setConfig in compile

--- 1compile.sh
+++ 1compile.sh
@@ -4,6 +4,7 @@
 setConfig () {
   key=$1
   value=$2
+  echo Config: $key = $value
   grep -q "CONFIG_$key" .config || exit 1
   sed -i "/CONFIG_$key[\=\ ]/d" .config
   echo "CONFIG_$key=$value" >> .config
@@ -20,12 +21,30 @@ prepOwrt() {
     scripts/feeds update
     scripts/feeds install kmod-batman-adv
     scripts/feeds install fastd
+    scripts/feeds install curl
 
     echo CONFIG_TARGET_$1=y > .config || exit1
     make defconfig || exit 1
 
     setConfig PACKAGE_kmod-batman-adv m
     setConfig PACKAGE_kmod-bridge m
+    setConfig PACKAGE_curl m
+    setConfig PACKAGE_ebtables m
+    setConfig PACKAGE_ip6tables m
+    setConfig PACKAGE_iptables m
+    setConfig PACKAGE_ip m
+    setConfig PACKAGE_fastd m
+    setConfig PACKAGE_6relayd n
+    setConfig PACKAGE_odhcp6c n
+    setConfig PACKAGE_hostapd m
+    setConfig PACKAGE_hostapd-utils m
+    setConfig PACKAGE_ppp m
+
+    make defconfig || exit 1
+
+    setConfig PACKAGE_kmod-ebtables-ipv4 m
+    setConfig PACKAGE_kmod-ebtables-ipv6 m
+    setConfig PACKAGE_ebtables-utils m
 
     make defconfig || exit 1
 
