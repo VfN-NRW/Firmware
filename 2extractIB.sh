6dd69150886db701a7a824a158917561905e71ee
xlizard <sb@liztv.net>
2014-06-29 20:38:24 +0200

    extract x64_64 imagebuilder

--- 2extractIB.sh
+++ 2extractIB.sh
@@ -7,8 +7,8 @@ tar xvjf imagebuilder/OpenWrt-ImageBuilder-ar71xx_generic-for-linux-x86_64.tar.b
 #cp ./openwrt/build_dir/target-powerpc_*/linux-mpc85xx_generic/linux-*/scripts/ OpenWrt-ImageBuilder-mpc85xx_generic-for-linux-x86_64/build_dir/target-powerpc_*/linux-mpc85xx_generic/linux-*/ -r
 #cp ./openwrt/build_dir/target-powerpc_*/linux-mpc85xx_generic/linux-*/arch/ OpenWrt-ImageBuilder-mpc85xx_generic-for-linux-x86_64/build_dir/target-powerpc_*/linux-mpc85xx_generic/linux-*/ -r
 
-#rm -rf OpenWrt-ImageBuilder-x86_kvm_guest-for-linux-x86_64
-#tar xvjf openwrt/bin/x86/OpenWrt-ImageBuilder-x86_kvm_guest-for-linux-x86_64.tar.bz2
+rm -rf OpenWrt-ImageBuilder-x86_64-for-linux-x86_64
+tar xvjf imagebuilder/OpenWrt-ImageBuilder-x86_64-for-linux-x86_64.tar.bz2
 #cp -R ./openwrt/staging_dir/host/lib/ OpenWrt-ImageBuilder-x86_kvm_guest-for-linux-x86_64/staging_dir/host/
 
 sed -i 's/# CONFIG_TARGET_ROOTFS_SQUASHFS is not set/CONFIG_TARGET_ROOTFS_SQUASHFS=y/' OpenWrt-ImageBuilder-*/.config 
