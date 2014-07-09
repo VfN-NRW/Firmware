7b54fac96d66c434e10ab857a723f16e064f60fc
xlizard <sb@liztv.net>
2014-07-09 20:34:56 +0200

    added batctl into images

--- 3makeImage.sh
+++ 3makeImage.sh
@@ -26,7 +26,7 @@ BUILD=$(($BUILD+1))
 echo $BUILD > build.txt
 
 
-CFGBASE='-6relayd -kmod-wpad -odhcp6c -odhcpd -wpad-mini fastd kmod-batman-adv ip curl ecdsautils ppp-mod-pppoe haveged socat tc kmod-sched-core kmod-sched'
+CFGBASE='-6relayd -kmod-wpad -odhcp6c -odhcpd -wpad-mini fastd kmod-batman-adv batctl ip curl ecdsautils ppp-mod-pppoe haveged socat tc kmod-sched-core kmod-sched'
 CFGHOTSPOT='hostapd kmod-ath'
 CFGx86='kmod-ide-core kmod-ide-generic kmod-block2mtd'
 
