eb5ab93c40bf39d6af23f068c2a7059034847fa6
Freifunk Firmware <firmware@liztv.net>
2014-07-09 21:07:28 +0200

    x86 > ext4

--- 3makeImage.sh
+++ 3makeImage.sh
@@ -55,7 +55,7 @@ makeimage "$CFGBASE $CFGHOTSPOT" ar71xx_generic 'MYNETN750' ar71xx-generic-mynet
 
 makeimage "$CFGBASE $CFGHOTSPOT" ar71xx_generic 'WZRHPAG300H' ar71xx-generic-wzr-hp-ag300h-squashfs-factory.bin ar71xx-generic-wzr-hp-ag300h-squashfs-sysupgrade.bin
 
-makeimage "$CFGBASE $CFGx86" x86_64 '' x86_64-combined-squashfs.img x86_64-combined-squashfs.img
-qemu-img convert -f raw -O vmdk images/openwrt-x86_64-combined-squashfs.img images/openwrt-x86_64-combined-squashfs.vmdk
+makeimage "$CFGBASE $CFGx86" x86_64 '' x86_64-combined-ext4.img.gz x86_64-combined-ext4.img.gz
+gzip -dk images/openwrt-x86_64-combined-ext4.img.gz && qemu-img convert -f raw -O vmdk images/openwrt-x86_64-combined-ext4.img images/openwrt-x86_64-combined-squashfs.vmdk && rm images/openwrt-x86_64-combined-ext4.img
 
 rename.ul openwrt- ff-nrw- images/*
