264f5b23eef1314421d170d3e0646a32f6e61b22
@RubenKelevra <cyrond@gmail.com>
2014-07-15 16:16:55 +0200

    fix profilname for wdr3600

--- 3makeImage.sh
+++ 3makeImage.sh
@@ -52,11 +52,12 @@ makeimage "$CFGBASE $CFGVPN $CFGHOTSPOT" ar71xx_generic 'TLWR941' ar71xx-generic
 makeimage "$CFGBASE $CFGVPN $CFGHOTSPOT" ar71xx_generic 'TLWR1043' ar71xx-generic-tl-wr1043nd-v1-squashfs-factory.bin ar71xx-generic-tl-wr1043nd-v1-squashfs-sysupgrade.bin
 makeimage "$CFGBASE $CFGVPN $CFGHOTSPOT" ar71xx_generic 'TLWR1043' ar71xx-generic-tl-wr1043nd-v2-squashfs-factory.bin ar71xx-generic-tl-wr1043nd-v2-squashfs-sysupgrade.bin
 
-makeimage "$CFGBASE $CFGVPN $CFGHOTSPOT" ar71xx_generic 'TLWDR3600' ar71xx-generic-tl-wdr3600-v1-squashfs-factory.bin ar71xx-generic-tl-wdr3600-v1-squashfs-sysupgrade.bin
+
 
 makeimage "$CFGBASE $CFGVPN $CFGHOTSPOT" ar71xx_generic 'TLMR3020' ar71xx-generic-tl-mr3020-v1-squashfs-factory.bin ar71xx-generic-tl-mr3020-v1-squashfs-sysupgrade.bin
 #makeimage "$CFGBASE $CFGVPN $CFGHOTSPOT" ar71xx_generic 'TLMR3040' ar71xx-generic-tl-mr3040-v1-squashfs-factory.bin ar71xx-generic-tl-mr3040-v1-squashfs-sysupgrade.bin
 
+makeimage "$CFGBASE $CFGVPN $CFGHOTSPOT" ar71xx_generic 'TLWDR4300' ar71xx-generic-tl-wdr3600-v1-squashfs-factory.bin ar71xx-generic-tl-wdr3600-v1-squashfs-sysupgrade.bin
 makeimage "$CFGBASE $CFGVPN $CFGHOTSPOT" ar71xx_generic 'TLWDR4300' ar71xx-generic-tl-wdr4300-v1-squashfs-factory.bin ar71xx-generic-tl-wdr4300-v1-squashfs-sysupgrade.bin
 #makeimage "$CFGBASE $CFGVPN $CFGHOTSPOT" mpc85xx_generic 'TLWDR4900' mpc85xx-generic-tl-wdr4900-v1-squashfs-factory.bin mpc85xx-generic-tl-wdr4900-v1-squashfs-sysupgrade.bin
 
