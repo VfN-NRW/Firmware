c6e03e8765b824f3d84c1729b8c480b80aeece21
xlizard <sb@liztv.net>
2014-08-07 23:51:51 +0200

    typo

--- 3makeImage.sh
+++ 3makeImage.sh
@@ -59,7 +59,7 @@ makeimage "$CFGBASE $CFGVPN $CFGHOTSPOT" "default" ar71xx_generic 'TLWR1043' ar7
 makeimage "$CFGBASE $CFGVPN $CFGHOTSPOT" "default" ar71xx_generic 'TLWR1043' ar71xx-generic-tl-wr1043nd-v2-squashfs-factory.bin ar71xx-generic-tl-wr1043nd-v2-squashfs-sysupgrade.bin
 
 
-makeimage "$CFGBASE $CFGVPN $CFGHOTSPOT" "default" ar71xx_generic 'TLWA901' openwrt-ar71xx-generic-tl-wa901nd-v2-squashfs-factory.bin ar71xx-generic-tl-wa901nd-v2-squashfs-sysupgrade.bin
+makeimage "$CFGBASE $CFGVPN $CFGHOTSPOT" "default" ar71xx_generic 'TLWA901' ar71xx-generic-tl-wa901nd-v2-squashfs-factory.bin ar71xx-generic-tl-wa901nd-v2-squashfs-sysupgrade.bin
 
 
 
