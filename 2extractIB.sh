c84eda8c40504791dde6515daf0307cad3af42bb
@RubenKelevra <cyrond@gmail.com>
2015-02-16 13:53:20 +0100

    Update 2extractIB.sh

--- 2extractIB.sh
+++ 2extractIB.sh
@@ -1,15 +1,15 @@
 #!./runByLine
-rm -rf OpenWrt-ImageBuilder-ar71xx_generic-for-linux-x86_64/
-tar xvjf imagebuilder/OpenWrt-ImageBuilder-ar71xx_generic-for-linux-x86_64.tar.bz2
+rm -rf OpenWrt-ImageBuilder-ar71xx_generic.linux-x86_64/
+tar xvjf imagebuilder/OpenWrt-ImageBuilder-ar71xx_generic.linux-x86_64.tar.bz2
 
-rm -rf OpenWrt-ImageBuilder-atheros-for-linux-x86_64/
-tar xvjf imagebuilder/OpenWrt-ImageBuilder-atheros-for-linux-x86_64.tar.bz2
+rm -rf OpenWrt-ImageBuilder-atheros.linux-x86_64/
+tar xvjf imagebuilder/OpenWrt-ImageBuilder-atheros.linux-x86_64.tar.bz2
 
-rm -rf OpenWrt-ImageBuilder-mpc85xx_generic-for-linux-x86_64/
-tar xvjf imagebuilder/OpenWrt-ImageBuilder-mpc85xx_generic-for-linux-x86_64.tar.bz2
+rm -rf OpenWrt-ImageBuilder-mpc85xx_generic.linux-x86_64/
+tar xvjf imagebuilder/OpenWrt-ImageBuilder-mpc85xx_generic.linux-x86_64.tar.bz2
 
-rm -rf OpenWrt-ImageBuilder-x86_64-for-linux-x86_64
-tar xvjf imagebuilder/OpenWrt-ImageBuilder-x86_64-for-linux-x86_64.tar.bz2
-cp -R ./openwrt/staging_dir/host/lib/ OpenWrt-ImageBuilder-x86_64-for-linux-x86_64/staging_dir/host/
+rm -rf OpenWrt-ImageBuilder-x86_64.linux-x86_64
+tar xvjf imagebuilder/OpenWrt-ImageBuilder-x86_64.linux-x86_64.tar.bz2
+cp -R ./openwrt/staging_dir/host/lib/ OpenWrt-ImageBuilder-x86_64.linux-x86_64/staging_dir/host/
 
 #sed -i 's/# CONFIG_TARGET_ROOTFS_SQUASHFS is not set/CONFIG_TARGET_ROOTFS_SQUASHFS=y/' OpenWrt-ImageBuilder-*/.config 
